0592f8c4924f8c29c69016451eb27d5f32118882
Alexander Agyapong <alex.agyapong@hotmail.com>
2021-01-24 15:14:29 +0000

    Add employee card (Initial)

---
+++ src/app/employee-card/employee-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Employee } from '../shared/models/employee';
+
+@Component({
+  selector: 'app-employee-card',
+  templateUrl: './employee-card.component.html',
+  styleUrls: ['./employee-card.component.scss']
+})
+export class EmployeeCardComponent implements OnInit {
+@Input() employee:Employee;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
